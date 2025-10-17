03bb29eeaca1ea6b9e94b9928295c58a28d0485c
Aniket-ux3 <aniketbarsinge@gmail.com>
2025-10-17 19:28:35 +0000

    Updated

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (_, res) => res.send('Hello from Jenkins + ngrok!!'));
+app.get('/', (_, res) => res.send('Hello from Jenkins + ngrok!! - Updated'));
 app.listen(3000, () => console.log('Running on 3000'));
